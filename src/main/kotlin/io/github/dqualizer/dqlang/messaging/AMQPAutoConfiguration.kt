9ca26e1b38e6ae97b9c010698fb8eb9abb92cfd2
LWagner <st148345@stud.uni-stuttgart.de>
2023-08-23 19:17:52 +0200

    Enable Spring AutoConfiguration

--- src/main/kotlin/io/github/dqualizer/dqlang/messaging/AMQPAutoConfiguration.kt
+++ src/main/kotlin/io/github/dqualizer/dqlang/messaging/AMQPAutoConfiguration.kt
@@ -1,14 +1,16 @@
 package io.github.dqualizer.dqlang.messaging
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.amqp.core.AmqpAdmin
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.support.converter.MessageConverter
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
-class AMQPConfiguration {
+class AMQPAutoConfiguration {
     @Bean
     fun messageConverter(): MessageConverter {
         return Jackson2JsonMessageConverter()
@@ -17,6 +19,20 @@ class AMQPConfiguration {
     @Bean
     fun objectMapper(): ObjectMapper {
         return ObjectMapper()
+    }
+
+    @Bean
+    fun queueConfiguration(): QueueConfiguration {
+        return QueueConfiguration()
+    }
+
+    @Bean
+    fun queueFactory(
+        applicationContext: ApplicationContext,
+        queueConfiguration: QueueConfiguration,
+        rabbitAdmin: AmqpAdmin
+    ): QueueFactory {
+        return QueueFactory(applicationContext, queueConfiguration, rabbitAdmin)
     }
 
 }
