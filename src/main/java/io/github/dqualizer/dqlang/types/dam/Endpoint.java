2f8afcc1756d269b337755522af3f3e3a8a1d21d
LWagner <st148345@stud.uni-stuttgart.de>
2023-08-25 15:00:14 +0200

    change lists to maps

--- src/main/java/io/github/dqualizer/dqlang/types/dam/Endpoint.java
+++ src/main/java/io/github/dqualizer/dqlang/types/dam/Endpoint.java
@@ -3,15 +3,15 @@ package io.github.dqualizer.dqlang.types.dam;
 import io.github.dqualizer.dqlang.types.rqa.definition.loadtest.PathVariable;
 import lombok.Data;
 
-import java.util.List;
+import java.util.Map;
 
 @Data
 public class Endpoint {
     private String field;
     private String operation;
-    private List<PathVariable> path_variables;
-    private List<Object> url_parameter;
-    private List<RequestParameter> request_parameter;
-    private List<Payload> payload;
-    private List<Response> responses;
+    private Map<PathVariable, String> path_variables;
+    private Map<Object, String> url_parameter;
+    private Map<RequestParameter, String> request_parameter;
+    private Map<Payload, String> payload;
+    private Map<Response, String> responses;
 }
