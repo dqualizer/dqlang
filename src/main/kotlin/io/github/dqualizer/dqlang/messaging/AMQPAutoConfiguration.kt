821fca48bae9c21a23c6e09c9363ca66a18dc79f
levinkerschberger <126667618+levinkerschberger@users.noreply.github.com>
2023-09-06 14:08:34 +0200

    Update AMQPAutoConfiguration.kt
    Changed ObjectMapper Behavior

--- src/main/kotlin/io/github/dqualizer/dqlang/messaging/AMQPAutoConfiguration.kt
+++ src/main/kotlin/io/github/dqualizer/dqlang/messaging/AMQPAutoConfiguration.kt
@@ -18,7 +18,7 @@ class AMQPAutoConfiguration {
 
     @Bean
     fun objectMapper(): ObjectMapper {
-        return ObjectMapper()
+        return ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false)
     }
 
     @Bean
