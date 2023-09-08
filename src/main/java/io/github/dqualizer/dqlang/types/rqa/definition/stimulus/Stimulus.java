bc72354fc4b5f8b2065af123dca6cd06f46226c7
Levin Kerschberger <levinkerschberger@novatec-gmbh.de>
2023-09-08 15:58:00 +0200

    Could work...

--- src/main/java/io/github/dqualizer/dqlang/types/rqa/definition/stimulus/Stimulus.java
+++ src/main/java/io/github/dqualizer/dqlang/types/rqa/definition/stimulus/Stimulus.java
@@ -1,5 +1,6 @@
 package io.github.dqualizer.dqlang.types.rqa.definition.stimulus;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
@@ -11,16 +12,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "load_profile")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
         @JsonSubTypes.Type(value = LoadPeakStimulus.class, name = "LOAD_PEAK"),
         @JsonSubTypes.Type(value = LoadIncreaseStimulus.class, name = "LOAD_INCREASE"),
         @JsonSubTypes.Type(value = ConstantLoadStimulus.class, name = "CONSTANT_LOAD")
 })    
 public abstract class Stimulus {
+
+    @JsonIgnore
+    private String type;
     @JsonProperty("load_profile")
     private LoadProfile loadProfile;
 
     private int accuracy;
+
+    public void setLoadProfile(LoadProfile loadProfile) {
+        this.loadProfile = loadProfile;
+        this.type = loadProfile.name();
+    }
 
 }
