a6563b5d7a50bec69ceec8a1664c3a5d1c4a5cb5
LWagner <st148345@stud.uni-stuttgart.de>
2023-10-08 16:21:18 +0200

    fix actor deserialization

--- src/main/java/io/github/dqualizer/dqlang/types/dam/domainstory/Actors.kt
+++ src/main/java/io/github/dqualizer/dqlang/types/dam/domainstory/Actors.kt
@@ -1,7 +1,15 @@
 package io.github.dqualizer.dqlang.types.dam.domainstory
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = Person::class, name = "Person"),
+    JsonSubTypes.Type(value = System::class, name = "System"),
+    JsonSubTypes.Type(value = Group::class, name = "Group")
+)
 sealed class Actor(actorName: String) : DSTEntity(actorName)
 
 data class Person @JsonCreator constructor(val personName: String) : Actor(personName)
