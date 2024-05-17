e4e30c529b465b21b3bf07a972052cf776e0fdd3
Henning Möllers <LeHenningo@users.noreply.github.com>
2024-05-17 15:10:25 +0200

    operationId --> methodPath

--- src/main/java/io/github/dqualizer/dqlang/types/dam/Activity.java
+++ src/main/java/io/github/dqualizer/dqlang/types/dam/Activity.java
@@ -12,9 +12,8 @@ public class Activity {
     @Id
     private String id;
     private String name;
-    // TODO RENAME packageMember
-    @JsonProperty("operation_id")
-    private String operationId;
+    @JsonProperty("method_path")
+    private String methodPath;
     private String initiator;
     private String workObject;
     private String action;
