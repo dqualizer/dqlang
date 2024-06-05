c5b1c454cabad4f0f749d614b7f8361a30bc7307
Jens Plüddemann <jens.plueddemann@novatec-gmbh.de>
2024-06-05 15:05:09 +0200

    disambiguate name

--- src/main/kotlin/io/github/dqualizer/dqlang/types/dam/domainstory/WorkObject.kt
+++ src/main/kotlin/io/github/dqualizer/dqlang/types/dam/domainstory/WorkObject.kt
@@ -2,9 +2,11 @@ package io.github.dqualizer.dqlang.types.dam.domainstory
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import org.springframework.data.mongodb.core.mapping.Field
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
 data class WorkObject(
+  @Field("workObjectName")
   override val name: String,
 
   val type: WorkObjectType,
